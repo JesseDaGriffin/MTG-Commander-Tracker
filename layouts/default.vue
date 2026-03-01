6f51f6bf90c94e9449fa6f90dfa62d7f3f5a6f34
jesse <jessegriffin123@gmail.com>
2026-02-28 21:46:51 -0800

    feat: add default layout component and update gitignore to exclude the Netlify local folder.

--- layouts/default.vue
+++ layouts/default.vue
@@ -105,6 +105,13 @@
                 <Icon name="mdi:sword-cross" class="text-2xl" />
                 <span class="text-[0.7rem] font-medium">Games</span>
             </NuxtLink>
+            <button
+                @click="signOut"
+                class="flex flex-col items-center justify-center gap-1 text-secondary w-full h-full transition-colors hover:text-mtg-red"
+            >
+                <Icon name="mdi:logout" class="text-2xl" />
+                <span class="text-[0.7rem] font-medium">Log Out</span>
+            </button>
         </nav>
     </div>
 </template>
